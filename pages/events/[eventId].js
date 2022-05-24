2aa06ead6e5c3bc0e9d8ff315771a1db99b4051d
Tor Henning Egenes <tor.henning.egenes@gmail.com>
2022-05-24 22:30:22 +0200

    added changes

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,7 +1,16 @@
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
 function EventDetailPage() {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <h1>No event found 😢</h1>;
+  }
   return (
     <div>
-      <h1>Specific event!</h1>
+      <h1>Event Detail</h1>
     </div>
   );
 }
